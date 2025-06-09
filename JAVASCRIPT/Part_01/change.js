420db1718a10f253d373a20a45451ae2fae85306
Harish Verma <hari734000@gmail.com>
2025-06-09 12:59:02 +0000

    start JavScript

---
+++ JAVASCRIPT/Part_01/change.js
@@ -0,0 +1,15 @@
+let getName = "harish verma";
+
+getName = "Manish";
+
+console.log(getName);
+
+const userId = 232323; //const value does not changed.
+
+// userId = 454545;
+
+// console.log(userId);
+// console.log(userId);
+
+
+ 
---
+++ JAVASCRIPT/Part_01/datatypes.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+/*
+String = hello world
+Number = 3,4,5,6,7,8
+Boolean = true/false
+BigInt = 34345n
+
+undefined = The value should be defined in the future
+null 
+Object
+Symbol
+
+*/
